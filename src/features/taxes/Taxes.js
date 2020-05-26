35fa9f516ade6097af826d56963c16b8afa40f46
Breno Capaz <aliayman@alumni.harvard.edu>
2020-05-26 13:54:03 -0700

    Taxes2020

--- src/features/taxes/Taxes.js
+++ src/features/taxes/Taxes.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Taxes = ()=> {
 
     return (
-        <iframe style={{width: '100%', height: '100vh'}} src="https://www.calculator.net/tax-calculator.html?cfilestatus=MarriedJoint&callowance=4&callowanceold=0&ctaxyear=2019&csalaryincome=100000&cwithheld=0&csalarystate=0&csalarylocal=0&chasbusiness=no&cbusinessincome=0&cestimatedtax=0&cmedicarewage=0&csalaryincome2=0&cwithheld2=0&csalarystate2=0&csalarylocal2=0&chasbusiness2=no&cbusinessincome2=0&cestimatedtax2=0&cmedicarewage2=0&cinterestincome=0&cordinarydividends=0&cqualifieddividends=0&crentalincome=0&cshortcapitalgain=0&clongcapitalgain=0&cotherincome=0&cstatetaxrate=0&cira=0&csalestax=6000&cmortgage=22000&cdonations=500&cstudentloan=0&cchildcare=0&ctuition=0&ctuition2=0&ctuition3=0&ctuition4=0&cotherdeductable=0&printit=0&x=70&y=29"></iframe>
+        <iframe style={{width: '100%', height: '100vh'}} src="https://www.calculator.net/tax-calculator.html?cfilestatus=MarriedJoint&callowance=4&callowanceold=0&ctaxyear=2020&csalaryincome=100000&cwithheld=0&csalarystate=0&csalarylocal=0&chasbusiness=no&cbusinessincome=0&cestimatedtax=0&cmedicarewage=0&csalaryincome2=0&cwithheld2=0&csalarystate2=0&csalarylocal2=0&chasbusiness2=no&cbusinessincome2=0&cestimatedtax2=0&cmedicarewage2=0&cinterestincome=0&cordinarydividends=0&cqualifieddividends=0&crentalincome=0&cshortcapitalgain=0&clongcapitalgain=0&cotherincome=0&cstatetaxrate=0&cira=0&csalestax=6000&cmortgage=22000&cdonations=500&cstudentloan=0&cchildcare=0&ctuition=0&ctuition2=0&ctuition3=0&ctuition4=0&cotherdeductable=0&printit=0&x=70&y=29"></iframe>
     )
 }
 
