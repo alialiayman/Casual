0707f51fdc5dbc0882b98863db8df6a0fa2d3f97
Breno Capaz <aliayman@alumni.harvard.edu>
2020-05-26 13:48:20 -0700

    Taxes

--- src/features/root/Root.js
+++ src/features/root/Root.js
@@ -5,6 +5,7 @@ import Algorithms from '../algorithms/Algorithms'
 import BinaryTree from '../binaryTree/BinaryTree'
 import Colors from '../colors/Colors';
 import HijriAge from '../hijriAge/HijriAge';
+import Taxes from '../taxes/Taxes';
 
 const Root = ()=> {
     return (
@@ -13,6 +14,7 @@ const Root = ()=> {
             <Route exact path="/" component={null}></Route>
             <Route exact path="/binarytree" component={BinaryTree}></Route>
             <Route exact path="/algorithms" component={Algorithms}></Route>
+            <Route exact path="/taxes" component={Taxes}></Route>
             <Route exact path="/colors" component={Colors}></Route>
             <Route exact path="/hijriage" component={HijriAge}></Route>
         </React.Fragment>
