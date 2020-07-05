01a5c730059aaf11cd4970c185478a5401e1eef7
Ayman Ali <aliayman@Aymans-MacBook-Pro13.local>
2020-07-04 23:51:27 -0700

    use color conversion library

--- src/features/colors/Colors.js
+++ src/features/colors/Colors.js
@@ -4,35 +4,19 @@ import Color from './components/Color'
 const Colors = () => {
     return (
         <React.Fragment>
-            <Color name='Lime' code='#05FF01'></Color>
-            <Color name='Olive' code='#818000'></Color>
-            <Color name='Skyblue' code='#86CEEB'></Color>
-            <Color name='Navy' code='#000080'></Color>
-            <Color name='Snow' code='#FFFAFA'></Color>
-            <Color name='Chocolate' code='#D2691F'></Color>
-            <Color name='Firebrick' code='#B22122'></Color>
-            <Color name='Tan' code='#D2B48C'></Color>
-            <Color name='Silver' code='#C0C0C0'></Color>
-            <Color name='Teal' code='#028080'></Color>
-            <div style={{ textAlign: 'center' , display: 'flex', flexDirection: 'column'}}>
-                <a href="https://visme.co/blog/color-combinations" target="_blank" rel="noopener noreferrer" >https://visme.co/blog/color-combinations</a>
-                <a href="https://colordrop.io">https://colordrop.io</a>
-                <a href="https://web.njit.edu/~walsh/index.php">https://web.njit.edu/~walsh/index.php</a>
-                <a href="http://hajonsoft.com/html/colors.html">http://hajonsoft.com/html/colors.html</a>
-                <a href="https://www.color-blindness.com/color-name-hue/">Color Name</a>
-                <a href="https://websitesetup.org/website-color-schemes/">Color for Industries</a>
-                <a href="https://www.crazyegg.com/blog/website-color-palettes/">The best Palettes</a>
-                <a href="https://material.io/design/color/the-color-system.html#color-usage-and-palettes">Color System</a>
-                <a href="https://webflow.com/blog/web-design-101-color-theory">Color Theory</a>
-                <a href="https://material.io/design/color/the-color-system.html#color-usage-and-palettes">Material color system</a>
-                <a href="https://mycolor.space/">Generate Palette</a>
-        <a href="https://material.io/resources/color/#!/?view.left=0&view.right=0">Material color viewer [UI]</a>
-        <a href="https://material.io/inline-tools/color/">Material inline color viewer [UI]</a>
-        
-        <a href="https://www.tailorbrands.com/blog/logo-color-combinations">color combinations</a>
-        
-            </div>
 
+            <Color name='Free speech blue - material primary' code='#3F51B5'></Color>
+            <Color name='Razzmatazz - material secondary' code='#F50057' url="https://material-ui.com/customization/default-theme/#default-theme" title="Default theme"></Color>
+            <Color name='Lime' code='#05FF01' url="https://visme.co/blog/color-combinations" title="Some beautiful colors"></Color>
+            <Color name='Olive' code='#818000' url="https://web.njit.edu/~walsh/index.php" title="New Jersy Institute"></Color>
+            <Color name='Skyblue' code='#86CEEB' url="http://hajonsoft.com/html/colors.html" title="HajOnSoft"></Color>
+            <Color name='Navy' code='#000080' url="https://www.color-blindness.com/color-name-hue" title="Color name"></Color>
+            <Color name='Snow' code='#FFFAFA' url="https://websitesetup.org/website-color-schemes/" title="Industries"></Color>
+            <Color name='Chocolate' code='#D2691F' url="https://material.io/design/color/the-color-system.html#color-usage-and-palettes" title="Material.io"></Color>
+            <Color name='Firebrick' code='#B22122' url="https://webflow.com/blog/web-design-101-color-theory" title="Web design color theory"></Color>
+            <Color name='Tan' code='#D2B48C' url="https://material.io/resources/color/#!/?view.left=0&view.right=0" title="Material color tool"></Color>
+            <Color name='Silver' code='#C0C0C0' url="https://material.io/inline-tools/color/" title="Material inline color viewer"></Color>
+            <Color name='Teal' code='#028080' url="https://www.tailorbrands.com/blog/logo-color-combinations" title="Logo harmony"></Color>
         </React.Fragment>
     )
 }
