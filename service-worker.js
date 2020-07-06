d86002e9a7e7da022023cd42146bd54f0591dd3e
Breno Capaz <aliayman@alumni.harvard.edu>
2020-07-05 17:03:02 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Casual/precache-manifest.02a24b420a56f3eabaf18f90e5d20e97.js"
+  "/Casual/precache-manifest.e666b1dd46c7b6d607001321123ba8a3.js"
 );
 
 self.addEventListener('message', (event) => {
